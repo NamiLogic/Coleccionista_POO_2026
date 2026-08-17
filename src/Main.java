11774bac2b834f4638d20253c101caad3647923d
NamiLogic <yulianabelch@gmail.com>
2026-08-17 10:34:23 -0400

    Vimos el concepto de encapsulamiento y abstraccion

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        mostrarMenu();
+
+    }
+    
+    static void mostrarMenu(){
+
         String opcion_elegida;
         Scanner scanner = new Scanner(System.in);
-
+        boolean iniciar_menu = true;
+        Personaje personaje_jugador = new Personaje();
         System.out.println("--BIENVENIDOS AL MUNDO DE WESTEROS");
+
+        while (iniciar_menu) {
 
         System.out.println("1.- Crear personaje");
         System.out.println("2.- Ver personaje");
@@ -19,19 +28,39 @@ public class Main {
         switch (opcion_elegida) {
             case "1":
                 System.out.println("CREACION DE PERSONAJE");
+                
+                System.out.println("¿Como llamaras a tu héroe?");
+                personaje_jugador.nombre = scanner.nextLine();
+
+                System.out.println("¿Cual es el apellido de tu heroe?");
+                personaje_jugador.apellido = scanner.nextLine();
+
+                System.out.println("¿Indique la vida del héroe?");
+                //Ya no lo haremos asi, por que es inseguro
+                //personaje_jugador.vida = Integer.parseInt(scanner.nextLine());
+                personaje_jugador.setearVida(Integer.parseInt (scanner.nextLine()));
                 break;
+
             case "2":
                 System.out.println("VER PERSONAJE");
+                System.out.println("Nombre: " + personaje_jugador.nombre);
+                System.out.println("Apellido: " + personaje_jugador.apellido);
+                System.out.println("Vida: " + personaje_jugador.verVida());
+
                 break;
+
             case "3":
                 System.out.println("GRACIAS POR JUGAR");
+                iniciar_menu = false;
                 break;
         
             default:
                 System.out.println("POR FAVOR INGRESE UNA DE LAS OPCIONES PERMITIDAS");
                 break;
         }
+        }
+
+        
 
     }
-    
 }
--- src/Personaje.java
+++ src/Personaje.java
@@ -1,8 +1,21 @@
 public class Personaje {
-    
+
     String nombre;
     String apellido;
-    int vida;
+    private int vida;
     boolean esta_vivo = true;
 
+
+    public void setearVida(int vida){
+        if(vida < 0){
+            this.vida = 0;
+        }
+        else {
+            this.vida = vida;
+        }
+    }
+
+    public int verVida(){
+        return this.vida;
+    }
 }
