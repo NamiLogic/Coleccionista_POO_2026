e4f48a63d26609faf989b9ebcbbbd730b7105123
NamiLogic <yulianabelchgmail.com>
2026-08-20 17:13:56 -0400

    Abstraccion y encapsulamiento

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,98 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        mostrarMenu();
+
+  public static void main(String[] args) {
+
+    mostrarMenu();
+  }
+
+  static void mostrarMenu(){
+
+    String opcion_menu;
+
+    boolean mantener_menu = true;
+
+    Personaje personaje_jugador = new Personaje();
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (mantener_menu) {
+
+      System.out.println("Elija su opción");
+
+      System.out.println("1.- Crear personaje");
+
+      System.out.println("2.- Ver personaje");
+
+      System.out.println("3.- Cerrar");
+
+      opcion_menu = scanner.nextLine();
+
+      switch (opcion_menu) {
+
+        case "1":
+
+          System.out.println("Creación del Personaje");
+
+          System.out.println("¿Cómo llamaras al personaje?");
+
+          personaje_jugador.nombre = scanner.nextLine();
+
+          System.out.println("¿Cómo llamaras al apellido del personaje?");
+
+          personaje_jugador.apellido = scanner.nextLine();
+
+          System.out.println("Asigna la vida del personaje");
+
+          personaje_jugador.setVida(Integer.parseInt(scanner.nextLine()));
+
+          System.out.println("Asignarle la edad al personaje");          
+
+          personaje_jugador.ingresarEdad(Integer.parseInt(scanner.nextLine()));
+
+          break;
+
+        case "2":
+
+          System.out.println("Datos del personaje creado");
+
+          System.out.println("Nombre personaje: " + personaje_jugador.nombre);
+
+          System.out.println("Apellido personaje: " + personaje_jugador.apellido);
+
+          //OJITO CON ESTO, ESTO ES LA LUZ!
+
+          //REFLEXIONA, POR QUÉ ES MEJOR ESTO?
+
+          //ENCAPSULANDO
+
+          System.out.println("Vida personaje: " + personaje_jugador.getVida());
+
+          System.out.println("¿Se encuentra vivo?: " + personaje_jugador.esta_vivo);
+
+          System.out.println("La edad del personaje es: " + personaje_jugador.obtenerEdad());
+
+          break;
+
+        case "3":
+
+          System.out.println("Cosas de opcion 3");
+
+          mantener_menu = false;
+
+          break;
+
+        default:
+
+          System.out.println("Por favor ingrese una opción valida");
+
+          break;
+
+      }
 
     }
-    
-    static void mostrarMenu(){
 
-        String opcion_elegida;
-        Scanner scanner = new Scanner(System.in);
-        boolean iniciar_menu = true;
-        Personaje personaje_jugador = new Personaje();
-        System.out.println("--BIENVENIDOS AL MUNDO DE WESTEROS");
+  }
 
-        while (iniciar_menu) {
-
-        System.out.println("1.- Crear personaje");
-        System.out.println("2.- Ver personaje");
-        System.out.println("3.- Salir");
-
-        System.out.println("Por favor ingrese una opcion: ");
-
-        opcion_elegida = scanner.nextLine();
-        System.out.print("La opcion escogida por el usuario es: " + opcion_elegida);
-
-        switch (opcion_elegida) {
-            case "1":
-                System.out.println("CREACION DE PERSONAJE");
-                
-                System.out.println("¿Como llamaras a tu héroe?");
-                personaje_jugador.nombre = scanner.nextLine();
-
-                System.out.println("¿Cual es el apellido de tu heroe?");
-                personaje_jugador.apellido = scanner.nextLine();
-
-                System.out.println("¿Indique la vida del héroe?");
-                //Ya no lo haremos asi, por que es inseguro
-                //personaje_jugador.vida = Integer.parseInt(scanner.nextLine());
-                personaje_jugador.setearVida(Integer.parseInt (scanner.nextLine()));
-                break;
-
-            case "2":
-                System.out.println("VER PERSONAJE");
-                System.out.println("Nombre: " + personaje_jugador.nombre);
-                System.out.println("Apellido: " + personaje_jugador.apellido);
-                System.out.println("Vida: " + personaje_jugador.verVida());
-
-                break;
-
-            case "3":
-                System.out.println("GRACIAS POR JUGAR");
-                iniciar_menu = false;
-                break;
-        
-            default:
-                System.out.println("POR FAVOR INGRESE UNA DE LAS OPCIONES PERMITIDAS");
-                break;
-        }
-        }
-
-        
-
-    }
 }
